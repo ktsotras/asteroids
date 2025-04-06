37b8a12500febbb10e68a4ef31ffd63885e81608
ktsotras <kevin.tsotras@gmail.com>
2025-04-06 18:27:04 -0400

    implemented 60 FPS limit

--- main.py
+++ main.py
@@ -11,14 +11,17 @@ def main():
     print(f"Screen height: {SCREEN_HEIGHT}")
 
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    clock = pygame.time.Clock()
+    dt = 0
 
-    while(pygame.get_init()):
+    while pygame.get_init():
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
         screen.fill("black")
 
         pygame.display.flip()
+        dt = clock.tick(60) / 1000
 
 if __name__ == "__main__":
     main()
