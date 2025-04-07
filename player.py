cbf4ab5934e192e6443c97cebb56c368b59bdaa0
ktsotras <kevin.tsotras@gmail.com>
2025-04-06 20:19:33 -0400

    player movement added

--- player.py
+++ player.py
@@ -28,3 +28,11 @@ class Player(CircleShape):
             self.rotate(-dt)
         if keys[pygame.K_d] or keys[pygame.K_RIGHT]:
             self.rotate(dt)
+        if keys[pygame.K_w] or keys[pygame.K_UP]:
+            self.move(dt)
+        if keys[pygame.K_s] or keys[pygame.K_DOWN]:
+            self.move(-dt)
+
+    def move(self, dt):
+        forward = pygame.Vector2(0, 1).rotate(self.rotation)
+        self.position += forward * PLAYER_SPEED * dt
